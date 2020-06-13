8aee67d39b96ed6d0e9321ebca3f2d1ae6f0fbac
nikitha <mnikitha365@gmail.com>
2020-06-13 15:40:32 +0530

    loops

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+var digit = [40,50,60,75,55,49]
+for(i=0;i<digit.length;i++)
+{
+    if(digit[i]%2==0){
+        console.log("Even : "+digit[i] +"fizz")
+    }
+    else
+    {
+        console.log("Odd :"+digit[i])
+    }
+}
+
+
+
+var x =6;
+var y =8;
+if(x<y)
+{
+    console.log("y is big")
+}
+else if(x>y)
+{
+    console.log("x is small")
+}
+var a =4;
+// switch(a){
+//     case 1:
+//         console.log("1 is executed");
+//         break;
+//     case 2:    
+//       console.log("2 is executed");
+//         break;
+//         case 3:    
+//       console.log("3 is executed");
+//         break;
+//         default:    
+//       console.log("default is executed");
+// }
